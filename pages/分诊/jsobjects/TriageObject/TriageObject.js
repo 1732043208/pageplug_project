ca8cda4c7bd88a8b33ed5e6286b601b672e4b078
黄 伟杰 <1732043208@qq.com>
2025-07-29 17:48:21 +0800

    多模态

--- pages/分诊/jsobjects/TriageObject/TriageObject.js
+++ pages/分诊/jsobjects/TriageObject/TriageObject.js
@@ -1,12 +1,29 @@
 export default {
 	InputValue : "",   //输入
 	answerValue:"",   //回答
+	filesList:[], //附件列表
+
+	fileLoad(files){
+		this.filesList = []
+		console.log('files',files)
+		files.forEach(item=>{
+			if(item.type.includes("image")) this.filesList.push({type:'image_url',image_url:{url:item.data}})
+		})
+		console.log('filesList', this.filesList)
+	},
+
 	changeInputValue(value){
 		console.log('value',value)
 		this.InputValue = value
 	},
+
 	async getCompletions(){
+		if(!this.InputValue) return showAlert("请输入您的症状！")
 		this.answerValue = ''
+		Commom.apiSearchContent = [
+			{type:'text',text:this.InputValue},
+			...this.filesList
+		]
 		const res = await completions.run()
 		console.log(res)
 		this.answerValue = res.choices[0].message.content
