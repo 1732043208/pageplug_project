0179e679ccb85993a4f70e1fc5488663ee56cad9
黄 伟杰 <1732043208@qq.com>
2025-07-29 10:21:27 +0800

    System generated commit, initial commit

---
+++ pages/分诊/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,19 @@
+export default {
+	colorValue: '1',
+	myFun1 (val) {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		console.log('11',val.selectedOptionValues)
+		this.colorValue = val.selectedOptionValues.join(',')
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	},
+	loadFile(val){
+		console.log('val',val);
+		console.log(FilePicker2)
+		console.log('ss',send.data)
+		console.log('hhh')
+	}
+}
