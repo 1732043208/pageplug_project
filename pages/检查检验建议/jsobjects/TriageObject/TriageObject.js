865c5d6a69e8a8142eda8751ee175df231e03254
黄 伟杰 <1732043208@qq.com>
2025-07-30 16:27:58 +0800

    添加检查检验建议

--- pages/检查检验建议/jsobjects/TriageObject/TriageObject.js
+++ pages/检查检验建议/jsobjects/TriageObject/TriageObject.js
@@ -1,25 +1,15 @@
 export default {
 	InputValue : "",   //输入
-	mainResults:"",   //主述结果
-	adviceResults:"", //建议结果
+	answerValue:"",   //回答
 	filesList:[], //附件列表
-	mainPrompt:`
-	患者主述：%InputValue%，根据患者主述，把主述分类，并给出类别标题。
-	要求如下：使用中文回复。
-	`, //主述结果prompt
-	advicePrompt:`
-	患者主述：%mainResults%，根据主述结果，开出检验/检查处方。
-	要求如下：使用中文回复。
-	`,//建议prompt
-	// prompt拼接替换
-	promptSplicing(prompt){
+	prompt:'患者叙述：%InputValue%，根据患者叙述，推荐一个最合适的科室',
+	promptSplicing(){
 		const replacements = {
 			'%InputValue%': this.InputValue,
-			'%mainResults%': this.mainResults,
 		}
 		return Object.entries(replacements).reduce(
 			(text, [pattern, replacement]) => text.replace(new RegExp(pattern), replacement),
-			prompt
+			this.prompt
 		)
 	},
 	fileLoad(files){
@@ -39,14 +29,32 @@ export default {
 	async getCompletions(){
 		if(!this.InputValue) return showAlert("请输入您的症状！")
 		Commom.apiSearchContent = [
-			{type:'text',text:this.promptSplicing(this.mainPrompt)},
+			{type:'text',text:this.promptSplicing()},
 			...this.filesList
 		]
 		const res = await completions.run()
 		console.log(res)
-		this.mainResults = res.choices[0].message.content
-		storeValue("InquiryMainResults", this.mainResults)
+		this.answerValue = res.choices[0].message.content
+
 		//重置上传组件
 		resetWidget("FilePicker1Copy", true);
+		/**
+		const res = await completions.run()
+		console.log(res)
+		// 正则表达式：匹配 "choices":[{"index":...,"delta":{"content":"(.*?)"
+		const regex = /"choices":\[\{"index":\d+,"delta":\{"content":"([^"]*)"/g;
+
+		// 使用 matchAll 获取所有匹配项
+		const matches = [...res.matchAll(regex)];
+
+		// 提取所有的 content 值
+		const contents = matches.map(match => match[1]);
+
+		// 打印所有 content 值
+		contents.forEach((content, index) => {
+			console.log(`${index + 1}. '${content}'`);
+			this.answerValue +=content
+		});
+		**/
 	},
 }
