07ecadd87d676c4e2cf12e91e3b5acac92f76e3b
黄 伟杰 <1732043208@qq.com>
2025-07-30 14:06:54 +0800

    更新

--- pages/问诊/jsobjects/InquiryObject/InquiryObject.js
+++ pages/问诊/jsobjects/InquiryObject/InquiryObject.js
@@ -1,8 +1,27 @@
 export default {
 	InputValue : "",   //输入
-	answerValue:"",   //回答
+	mainResults:"",   //主述结果
+	adviceResults:"", //建议结果
 	filesList:[], //附件列表
-
+	mainPrompt:`
+	患者主述：%mainPrompt%，根据患者主述，把主述分类，并给出类别标题。
+	要求如下：使用中文回复。
+	`, //主述结果prompt
+	advicePrompt:`
+	患者主述：%advicePrompt%，根据主述结果，开出检验/检查处方。
+	要求如下：使用中文回复。
+	`,//建议prompt
+	// prompt拼接替换
+	promptSplicing(prompt){
+		const replacements = {
+			'%mainPrompt%': this.mainPrompt,
+			'%advicePrompt%': this.advicePrompt,
+		}
+		return Object.entries(replacements).reduce(
+			(text, [pattern, replacement]) => text.replace(new RegExp(pattern), replacement),
+			prompt
+		)
+	},
 	fileLoad(files){
 		this.filesList = []
 		console.log('files',files)
@@ -19,14 +38,14 @@ export default {
 
 	async getCompletions(){
 		if(!this.InputValue) return showAlert("请输入您的症状！")
-		this.answerValue = ''
+		this.mainResults = ''
 		Commom.apiSearchContent = [
-			{type:'text',text:this.InputValue},
+			{type:'text',text:this.promptSplicing(this.mainPrompt)},
 			...this.filesList
 		]
 		const res = await completions.run()
 		console.log(res)
-		this.answerValue = res.choices[0].message.content
+		this.mainResults = res.choices[0].message.content
 
 		//重置上传组件
 		resetWidget("Input1Copy", true);
@@ -51,4 +70,14 @@ export default {
 		});
 		**/
 	},
+	async getAdvice(){
+		Commom.apiSearchContent = [
+			{type:'text',text:this.promptSplicing(this.advicePrompt)},
+		]
+		const res = await completions.run()
+		console.log('res',res)
+		this.adviceResults = res.choices[0].message.content
+		console.log('this.adviceResults',this.adviceResults)
+		showModal('Modal2');
+	}
 }
