1b2d4bf1099a539f80ca9a5ea6f016883bf0c661
黄 伟杰 <1732043208@qq.com>
2025-08-08 14:51:00 +0800

    md格式组件封装

--- pages/问诊详情/jsobjects/InquiryDetail/InquiryDetail.js
+++ pages/问诊详情/jsobjects/InquiryDetail/InquiryDetail.js
@@ -1,6 +1,7 @@
 export default {
 	created () {
-		console.log('11111',global.URL.queryParams)
-		console.log(moment().format('YYYY-MM-DD HH:mm:ss'))
+		Consultation.run().then(res=>{
+			console.log('res',res)
+		})
 	},
 }
