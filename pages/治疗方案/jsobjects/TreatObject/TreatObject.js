2ba08e3f78bdfda0d420c8a8f1e96b8f01024417
黄 伟杰 <huangwj@gdngsk.com>
2025-08-20 16:14:23 +0800

    更新

--- pages/治疗方案/jsobjects/TreatObject/TreatObject.js
+++ pages/治疗方案/jsobjects/TreatObject/TreatObject.js
@@ -7,12 +7,12 @@ export default {
 	ImgActive:null, //附件列表高亮索引
 
 	//prompt拼接
-	promptSplicing(InquiryMainResults, InspectionAdvice, DiagnosisAdvice, knowledgeAnswer, RequireType) {
+	promptSplicing(knowledgeAnswer, RequireType) {
 		const replacements = {
 			'%InputValue%': this.InputValue,
-			'%InquiryMainResults%': InquiryMainResults,
-			'%InspectionAdvice%': InspectionAdvice,
-			'%DiagnosisAdvice%': DiagnosisAdvice,
+			'%InquiryMainResults%':  global.store.InquiryMainResults,
+			'%InspectionAdvice%':  global.store.InspectionAdvice,
+			'%DiagnosisAdvice%':  global.store.DiagnosisAdvice,
 			'%RequireType%': RequireType,
 			'%patientStatement%': global.store.patientStatement, //患者主述
 			'%knowledgeAnswer%': knowledgeAnswer,
@@ -144,5 +144,39 @@ export default {
 		}catch(error){
 			showAlert('数据写入失败！', 'error')
 		}
+	},
+	// 模型对话传参处理
+	modelParamsHandle(knowledgeAnswer){
+		// prompt拼接
+		const measurePrompt = this.promptSplicing(knowledgeAnswer, '治疗措施')
+		console.log('prompt内容：', text)
+		const text = this.promptSplicing(knowledgeAnswer, '用药处方')
+		console.log('prompt内容：', text)
+
+		// 往模型对话列表添加询问记录（治疗措施）
+		Commom.modelSearchList1.push({"role":"user","content": [
+			{type:'text',text},
+			...this.filesList
+		]})
+
+		// 模型对话传参-判断是否有启用上下文（治疗措施）
+		Commom.modelSearchContent1 = context_Swtich.isSwitchedOn 	?  Commom.modelSearchList1  : 
+		[{"role":"user","content": [
+			{type:'text',text},
+			...this.filesList
+		]}]
+
+		// 往模型对话列表添加询问记录（用药方法）
+		Commom.modelSearchList2.push({"role":"user","content": [
+			{type:'text',text},
+			...this.filesList
+		]})
+
+		// 模型对话传参-判断是否有启用上下文（用药方法）
+		Commom.modelSearchContent2 = context_Swtich.isSwitchedOn 	?  Commom.modelSearchList2  : 
+		[{"role":"user","content": [
+			{type:'text',text},
+			...this.filesList
+		]}]
 	}
 }
